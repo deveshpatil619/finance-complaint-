0b6523b9529ede260a3e32eeb678b2a6fb8d5caf
deveshpatil619 <deveshpatil619@gmail.com>
2023-01-31 10:40:27 +0530

    updates

--- infrastructure/module.tf
+++ infrastructure/module.tf
@@ -1,10 +1,16 @@
+## The code is written in Terraform and is used to define the infrastructure for a financial application. 
+## The code has three main components:
+
 terraform {
-  backend "s3" {
+  backend "s3" {  ## The backend provides a central location to store the Terraform state, which helps maintain consistency and prevent conflicts. 
     bucket = "finance-tf-state"
     key    = "tf_state"
     region = "ap-south-1"
   }
 }
+
+# These modules represent different components of the infrastructure and are defined in separate Terraform
+# files in the "./finance_artifact_repository", "./finance_model_bucket", and "./finance_virtual_machine" directories, respectively 
 
 module "finance_artifact_repository" {
   source = "./finance_artifact_repository"
